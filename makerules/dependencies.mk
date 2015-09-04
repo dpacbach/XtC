67ced9f1a5cd1f5b5624a4e52651fe8cdceb077c
David P. Sicilia <dpsicilia@gmail.com>
2015-09-04 09:45:30 -0700

    Fix bug in dependencies file

--- makerules/dependencies.mk
+++ makerules/dependencies.mk
@@ -3,10 +3,10 @@ ALL_LOCATIONS := CMD TEST LIB LIB_INT
 #assert_headers = $(call assert,$(LOCATION_$1),"LOCATION_$1 NOT DEFINED")
 #$(call map,assert_headers,$(ALL_LOCATIONS))
 
-DEPS_CMD     = LIB_INT
-DEPS_TEST    = LIB_INT
-DEPS_LIB     = LIB_INT
-DEPS_LIB_INT =
+DEPS_CMD     := LIB_INT
+DEPS_TEST    := LIB_INT
+DEPS_LIB     := LIB_INT
+DEPS_LIB_INT :=
 
 include_flag = -I$(LOCATION_$1)
 
@@ -14,8 +14,8 @@ include_flag = -I$(LOCATION_$1)
 include_flags = $(call map,include_flag,$1 $(DEPS_$1))
 
 #$(call map,include_flags,$(ALL_LOCATIONS))
-INCLUDES_CMD  = $(call include_flags,$(DEPS_CMD))
-INCLUDES_TEST = $(call include_flags,$(DEPS_TEST))
-INCLUDES_LIB  = $(call include_flags,$(DEPS_LIB))
+INCLUDES_CMD  = $(call include_flags,CMD)
+INCLUDES_TEST = $(call include_flags,TEST)
+INCLUDES_LIB  = $(call include_flags,LIB)
 
 #$(foreach i,$(ALL_LOCATIONS),$(info INCLUDES_$(i)=$(INCLUDES_$(i))))
